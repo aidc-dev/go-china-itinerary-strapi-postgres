d0afa1a1f579d679fcdd163af5b175c1544010fe
Li,Ke <lk400248@aidcaib.com>
2025-07-06 15:36:32 +0800

    chore: add database pool setting

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -11,7 +11,20 @@ module.exports = ({ env }) => {
         port,
         database,
         user,
-        password
+        password,
+      },
+      pool: {
+        min: env.int("DATABASE_POOL_MIN", 2),
+        max: env.int("DATABASE_POOL_MAX", 10),
+        acquireTimeoutMillis: env.int("DATABASE_POOL_ACQUIRE_TIMEOUT", 60000),
+        createTimeoutMillis: env.int("DATABASE_POOL_CREATE_TIMEOUT", 30000),
+        destroyTimeoutMillis: env.int("DATABASE_POOL_DESTROY_TIMEOUT", 5000),
+        idleTimeoutMillis: env.int("DATABASE_POOL_IDLE_TIMEOUT", 30000),
+        reapIntervalMillis: env.int("DATABASE_POOL_REAP_INTERVAL", 1000),
+        createRetryIntervalMillis: env.int(
+          "DATABASE_POOL_CREATE_RETRY_INTERVAL",
+          200
+        ),
       },
       debug: false,
     },
